210b65f5a02c9391342912512ebed10365577c5b
Riccardo Milazzo <milazzoriccardo18@gmail.com>
2026-06-16 16:49:16 +0200

    .

---
+++ old/config/stripe.php
@@ -0,0 +1,11 @@
+<?php
+
+function getStripeConfig(): array
+{
+    return [
+        'secretKey' => '',
+        'publishableKey' => 'pk_test_51TZZ7fGu9c2BhXboiuHFoXW3s8v8z07Z2U1VJmXafo1TMMcofajhjx3rpDRaJ5LbtwjAEkWOykbxgvMDUMlGflnD001ivQgy2T',
+        'webhookSecret' => '',
+        'currency' => 'eur',
+    ];
+}
